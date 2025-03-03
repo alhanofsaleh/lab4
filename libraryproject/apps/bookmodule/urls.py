8b78669b57fa533386757e8ad206f6bccdf678c4
alhanofsaleh <xf.4f@hotmail.com>
2025-03-04 00:29:06 +0300

    Fix: Re-uploading missing files

---
+++ libraryproject/apps/bookmodule/urls.py
@@ -0,0 +1,10 @@
+﻿﻿from django.urls import path
+from .views import index, list_books, aboutus
+
+app_name = 'books' 
+
+urlpatterns = [
+    path('', index, name='index'),   
+    path('list/', list_books, name='list_books'),
+    path('about/', aboutus, name='aboutus'),
+]
---
+++ libraryproject/libraryproject/urls.py
@@ -0,0 +1,12 @@
+﻿from django.contrib import admin
+from django.urls import include, path
+from apps.bookmodule import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),  
+    path('', views.index, name='bookmodule.index'),  
+    path('list_books/', views.list_books, name='bookmodule.list_books'),
+    path('aboutus/', views.aboutus, name='bookmodule.aboutus'),
+    path('<int:bookId>/', views.viewbook, name='bookmodule.view_one_book'),
+    path('books/', include('apps.bookmodule.urls')),  
+]
